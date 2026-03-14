dcdbab18c3ed3a6979ad42f7eb145063092bc063
chris <chris@ele.rs>
2026-03-14 20:19:32 +0100

    Add basic Spectre.Console chat loop

--- src/ccode/Program.cs
+++ src/ccode/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Spectre.Console;
+
+while (true)
+{
+    var input = AnsiConsole.Prompt(
+        new TextPrompt<string>("[green]You[/]:")
+            .AllowEmpty());
+
+    if (string.IsNullOrWhiteSpace(input))
+        continue;
+
+    if (input.Equals("exit", StringComparison.OrdinalIgnoreCase))
+        break;
+
+    AnsiConsole.MarkupLine("[blue]Agent:[/] No functionality has been implemented yet.");
+    AnsiConsole.WriteLine();
+}
